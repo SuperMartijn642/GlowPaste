ce4057ccb06e935f837e91b102d78b5a013c221f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-23 14:11:14 +0200

    Allow left-clicking to remove glow paste

--- src/main/java/com/supermartijn642/glowpaste/content/GlowPasteItem.java
+++ src/main/java/com/supermartijn642/glowpaste/content/GlowPasteItem.java
@@ -3,6 +3,9 @@ package com.supermartijn642.glowpaste.content;
 import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.core.item.BaseItem;
 import com.supermartijn642.core.item.ItemProperties;
+import com.supermartijn642.core.util.Pair;
+import com.supermartijn642.glowpaste.GlowPaste;
+import com.supermartijn642.glowpaste.content.packets.HitGlowingPacket;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -11,8 +14,11 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 /**
@@ -39,6 +45,22 @@ public class GlowPasteItem extends BaseItem {
             return InteractionFeedback.SUCCESS;
         }
         return InteractionFeedback.PASS;
+    }
+
+    public boolean leftClickBlock(Player player, ItemStack stack, BlockPos pos, Direction side){
+        Level level = player.level();
+        GlowingBlockStorage glowingBlocks = GlowingBlockStorage.get(level);
+        if(!glowingBlocks.has(pos.getX(), pos.getY(), pos.getZ()))
+            return false;
+        if(level.isClientSide()){
+            GlowPaste.CHANNEL.sendToServer(new HitGlowingPacket(pos, side));
+            player.playSound(level.getBlockState(pos).getSoundType().getHitSound());
+        }else{
+            glowingBlocks.clear(pos.getX(), pos.getY(), pos.getZ());
+            if(!player.preventsBlockDrops())
+                Block.popResourceFromFace(level, pos, side, GlowPaste.glowPaste.getDefaultInstance());
+        }
+        return true;
     }
 
     @Override
